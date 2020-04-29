24fd44af208101775fac23cc3e20409721b24f22
Brad Donakowski <braddonako@gmail.com>
2020-04-29 11:31:16 -0600

    node is now collecting the 10 tweets. Going to try and create a simple front end

--- app.js
+++ app.js
@@ -17,7 +17,20 @@ let params = {
 
 T.get('search/tweets', params, function(err, data, response){
     if(!err){
-
+        for (let i = 0; i < data.statuses.length; i++){
+            // looping through the 10 tweets I am retrieving, then going to grab their id
+            let id = { id: data.statuses[i].id_str}
+            // going to create a post route to favorite the tweet
+            T.post('favorites/create', id, function(err, response){
+                if (err){
+                    console.log(err[0].message)
+                } else {
+                    let username = response.user.screen_name;
+                    let tweetId = response.id_str;
+                    console.log('favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+                }
+            })
+        }
     } else {
         console.log(err)
     }
